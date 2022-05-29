cf5fcb4d171aec3406dc472cdb40d2f81b46b44f
Aidan Chang <84875731+aidanschang@users.noreply.github.com>
2022-05-29 15:16:17 -0700

    Add files via upload

---
+++ AssignmentThree.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import UserProfile.UserProfile;
+
+public class AssignmentThree {
+
+	public static void main(String[] args) throws IOException {
+		//Aidan's file directory: "/Users/aidanpro/Documents/Programming/Eclipse Java/CS622_HW3/AssignmentThree/"
+		
+		UserProfile aidan= new UserProfile("Aidan");
+	}
+
+	
+}
